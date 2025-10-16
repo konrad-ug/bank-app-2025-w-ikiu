e32c219fd3e8f03b5baa3b26e03393f24b880df4
w-ikiu <chelminska.wiktoria@wp.pl>
2025-10-16 15:53:13 +0200

    feature 3 i 4

--- src/account.py
+++ src/account.py
@@ -1,6 +1,14 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel):
+    def __init__(self, first_name, last_name, pesel, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 0.0
-        self.pesel = pesel
+
+        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+
+        self.balance = 50 if self.is_promo_code_valid(promo_code) else 0.0
+
+    def is_pesel_valid(self, pesel):
+        return isinstance(pesel, str) and len(pesel) == 11
+    
+    def is_promo_code_valid(self, promo_code):
+        return isinstance(promo_code, str) and promo_code.startswith("PROM_") and len(promo_code) == 8
