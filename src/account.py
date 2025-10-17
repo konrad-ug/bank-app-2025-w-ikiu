ad42314dabad8c1db9afaa0df1e95821885f0d4a
w-ikiu <chelminska.wiktoria@wp.pl>
2025-10-17 11:34:15 +0200

    feature 5

--- src/account.py
+++ src/account.py
@@ -5,10 +5,22 @@ class Account:
 
         self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
 
-        self.balance = 50 if self.is_promo_code_valid(promo_code) else 0.0
+        self.balance = 50 if self.can_get_promo(pesel) and self.is_promo_code_valid(promo_code) else 0.0
 
     def is_pesel_valid(self, pesel):
         return isinstance(pesel, str) and len(pesel) == 11
     
     def is_promo_code_valid(self, promo_code):
         return isinstance(promo_code, str) and promo_code.startswith("PROM_") and len(promo_code) == 8
+    
+    def can_get_promo(self, pesel):
+        if not self.is_pesel_valid(pesel):
+            return False
+        yy, mm = int(pesel[:2]), int(pesel[2:4])
+        if 1 <= mm <= 12:
+            year = 1900 + yy
+        elif 21 <= mm <= 32:
+            year = 2000 + yy
+        else:
+            return False
+        return year > 1960        
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -9,6 +9,8 @@ class TestAccount:
         assert account.balance == 0.0
         assert account.pesel == "12345678900"
 
+    # testy sprawdzajace poprawnosc peselu
+
     def test_pesel_too_long(self):
         account = Account("Jane", "Doe", "1234567890000")
         assert account.pesel == "Invalid"
@@ -21,6 +23,8 @@ class TestAccount:
         account = Account("Jane", "Doe", "")
         assert account.pesel == "Invalid"
 
+    # testy do sprawdzenia podanego kodu promocyjnego
+
     def test_promo_code_suffix_too_long(self):
         account = Account("Alice", "Smith", "12345678900", "PROM_XYZZ")
         assert account.balance == 0
@@ -31,4 +35,34 @@ class TestAccount:
 
     def test_promo_code_prefix_wrong(self):
         account = Account("Alice", "Smith", "12345678900", "P_XYZ")
+        assert account.balance == 0
+
+    def test_promo_code_wrong(self):
+        account = Account("Alice", "Smith", "12345678900", "asdhaj")
+        assert account.balance == 0
+
+    # testy do walidacji roku urodzenia przy przyznawaniu promocji
+
+    def test_promo_code_too_old(self):
+        account = Account("Alice", "Smith", "59010100000", "PROM_XYZ")
+        assert account.balance == 0
+
+    def test_promo_code_too_old_1910(self):
+        account = Account("Alice", "Smith", "10100512345", "PROM_XYZ")
+        assert account.balance == 0
+
+    def test_promo_code_too_old_and_wrong_code(self):
+        account = Account("Alice", "Smith", "60010112345", "djsjk")
+        assert account.balance == 0
+
+    def test_promo_code_age_ok(self):
+        account = Account("Alice", "Smith", "75041512345", "PROM_XYZ")
+        assert account.balance == 50
+
+    def test_promo_code_age_ok_2010(self):
+        account = Account("Alice", "Smith", "75041512345", "PROM_XYZ")
+        assert account.balance == 50
+    
+    def test_promo_code_age_ok_wrong_code(self):
+        account = Account("Alice", "Smith", "02270312345", "PROM_XY")
         assert account.balance == 0
